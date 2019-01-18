5727a35fe1481d15bcecaf253a90c7b939589fbb
Tracy Reith <treith@treith.com>
2019-01-18 11:43:55 -0800

    Add content for last 2 questions, add js code for first two screening questions

--- track-suggester/js/scripts.js
+++ track-suggester/js/scripts.js
@@ -1,11 +1,31 @@
 $(document).ready(function(){
   $("form#questions").submit(function(event){
 
+    var frontBack = $("select#frontBack").val();
+    var patience = $("select#patience").val();
+
+    if ( patience === "nopatience") {
+      console.log(patience);
+      $(".container-fluid").children().toggle();
+      $(".nopatience").show();
+    } else if (frontBack === "front") {
+      $(".container-fluid").children().toggle();
+        console.log(frontBack);
+      $(".courses").show();
+      $(".design").show();
+    // } else if () {
+
+    } else {
+      $(".container-fluid").children().toggle();
+      $(".notsure").show();
+    }
+
     event.preventDefault();
   });
-  $("form#redo").click(function(event) {
+  $("button#redo").click(function(event) {
      $(".container-fluid").children().toggle();
      setTimeout(function () {
        location.reload()}, 10);
      event.preventDefault();
-});
+   });
+ });
